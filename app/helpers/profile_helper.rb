4eee278746c48eb39e26a7b245665668d0db0288
andydenenberg <andydenenberg@gmail.com>
2011-12-08 10:42:02 -0600

    tuned up the home page

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,8 +1,8 @@
 module ProfileHelper
-	
-	def show_photo(userp)
+  	
+	def show_photo(userp,size)
 	  if userp.avatar.file?
-		  image_tag userp.avatar.url(:thumb)
+		  image_tag userp.avatar.url(size)
 	  else
 		  data = 'No Photo on file'
     end	
